4d19b13101632e3de6c8d7fa4f675d50504485a0
xiaoran <xiaoran2495897824@163.com>
2024-01-24 17:12:26 +0800

    add utime

---
+++ util/utime.c
@@ -0,0 +1,15 @@
+#include "utime.h"
+
+double time_diff(struct timeval start, struct timeval end){
+    long seconds = end.tv_sec - start.tv_sec;
+    long useconds = end.tv_usec - start.tv_usec;
+    double time_use = (double) seconds + (double) useconds / 10e6;
+    return time_use;
+}
+
+long time_diff_us(struct timeval start, struct timeval end){
+    long seconds = end.tv_sec - start.tv_sec;
+    long useconds = end.tv_usec - start.tv_usec;
+    long time_use_us = seconds * 1000000 + useconds;
+    return time_use_us;
+}
---
+++ util/utime.h
@@ -0,0 +1,4 @@
+#include <sys/time.h>
+
+double time_diff(struct timeval start, struct timeval end);
+long time_diff_us(struct timeval start, struct timeval end);
